a7cb77e55bfe2427bd9c93610010fafd21339265
obsoletedevgit <theohaines@yahoo.com>
2026-02-07 13:34:49 +0000

    Chore: update visual identity

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,10 +2,12 @@ import { initUI } from "./utils/ui.js";
 import { initStorage } from "./utils/storage.js";
 import { initComparison } from "./utils/compare.js";
 import { initCalculator } from "./utils/calculator.js";
+import { initTheme } from "./utils/theme.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	initUI();
 	initStorage();
 	initCalculator();
 	initComparison();
+    initTheme();
 });
